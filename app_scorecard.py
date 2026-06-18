e43536a14e2f5d8f00f64d966680ad0520bc964e
majoocharteKellanova <mariajose.ocharte@kellanova.com>
2026-06-18 11:22:27 -0600

    Create app_scorecard.py

---
+++ app_scorecard.py
@@ -0,0 +1,34 @@
+# importar librerías básicas
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+
+# tantito backend
+hoy = datetime.today()
+meses = {
+    1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
+    5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
+    9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+
+st.image(
+"link-git.png",
+width=160
+)
+
+st.markdown(
+"COSTCO SCORECARD",
+unsafe_allow_html=True
+)
+
+# botón y variable para guardar el archivo
+info = st.file_uploader("sube aquí la información de inventarios", type=["csv"], accept_multiple_files=False)
+
+# ponemos la condicional del archivo para seguir el proceso
+if info: # una lista vacía se considera False y una con elementos True (recorderis)
+  df = pd.read_csv(info, skiprows=2)
+
+# preview del dataframe
+st.markdown("inventarios semanal", unsafe_allow_html=True)
+st.dataframe(df.head())
+
+  
