187a0cfc3bc9504ccd640e733a66033d54c66d3a
majoocharteKellanova <mariajose.ocharte@kellanova.com>
2026-06-18 12:19:02 -0600

    Update app_scorecard.py

--- app_scorecard.py
+++ app_scorecard.py
@@ -61,7 +61,7 @@ info = st.file_uploader("sube aquí la información de inventarios", type=["csv"
 
 # ponemos la condicional del archivo para seguir el proceso
 if info: # una lista vacía se considera False y una con elementos True (recorderis)
-    df = pd.read_csv(info, skiprows=1)
+    df = pd.read_csv(info, skiprows=2)
     
     # detectar columnas de fechas
     cols_fecha = [c for c in df.columns if c.startswith("1 Semana terminando el")]
