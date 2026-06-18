cae335e61d1d891bafdfc6f0e110b51226fd81fe
majoocharteKellanova <mariajose.ocharte@kellanova.com>
2026-06-18 11:29:46 -0600

    Update app_scorecard.py

--- app_scorecard.py
+++ app_scorecard.py
@@ -8,7 +8,7 @@ hoy = datetime.today()
 meses = {
     1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
     5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
-    9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+    9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre" }
 
 st.image(
 "link-git.png",
