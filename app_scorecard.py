9deeace4b1ca63368d33550d9daa0a6831299e72
majoocharteKellanova <mariajose.ocharte@kellanova.com>
2026-06-18 11:30:31 -0600

    Update app_scorecard.py

--- app_scorecard.py
+++ app_scorecard.py
@@ -10,10 +10,10 @@ meses = {
     5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
     9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre" }
 
-st.image(
-"link-git.png",
-width=160
-)
+# # st.image(
+# "link-git.png",
+# width=160
+# )
 
 st.markdown(
 "COSTCO SCORECARD",
