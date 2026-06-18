44a1d15cedbf7df0b5afc0381d5be78e4c0ba0f0
majoocharteKellanova <mariajose.ocharte@kellanova.com>
2026-06-18 11:55:30 -0600

    Update app_scorecard.py

--- app_scorecard.py
+++ app_scorecard.py
@@ -26,9 +26,18 @@ info = st.file_uploader("sube aquí la información de inventarios", type=["csv"
 
 # ponemos la condicional del archivo para seguir el proceso
 if info: # una lista vacía se considera False y una con elementos True (recorderis)
-    df = pd.read_csv(info, skiprows=2)
+    df = pd.read_csv(info, skiprows=1)
+    
+    # detectar columnas de fechas
+    cols_fecha = [c for c in df.columns if c.startswith("1 Semana terminando el")]
+
+    # # reemplazar las columnas repetidas por una nueva variable
+    # for c in cols_fecha:
+    #     df['Semana'] = 
+        
     # preview del dataframe
     st.markdown("inventarios semanal", unsafe_allow_html=True)
     st.dataframe(df.head())
+
 else:
     st.info("carga un archivo para comenzar")
